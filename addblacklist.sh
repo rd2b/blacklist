0a4ec23ded9b2ef18666416595a569c9be46b787
Remi Debay <rdebay.ext@orange.com>
2015-01-26 12:41:15 +0100

    Amelioration creation de blacklist

--- addblacklist.sh
+++ addblacklist.sh
@@ -11,8 +11,10 @@ set -u
 
 authfile="/var/log/auth.log"
 whitelist="/etc/myipsets/whitelist.d/*"
-black="/etc/myipsets/blacklist.d/authfailed_$(date +%F).txt"
+blacklist="/etc/myipsets/blacklist.d/*"
 
 . /etc/default/myblacklist
+
+black="$blacklist/authfailed_$(date +%F).txt"
 
 grep Failed $authfile | grep -v -f $whitelist | grep   -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" |sort -u >> $black
